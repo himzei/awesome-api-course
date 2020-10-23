04bb2b591d1206aa5ff5671e2ab16e62bac04c15
himzei <himzei@himzeiui-MacBookPro.local>
2020-10-23 15:21:30 +0900

    27 meview put

--- users/serializers.py
+++ users/serializers.py
@@ -19,3 +19,18 @@ class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User 
         exclude = ("groups", "user_permissions", "password", "last_login", "is_superuser", "is_staff", "is_active", "date_joined", "favs")
+
+
+class WriteUserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User 
+        fields = (
+            "username", 
+            "first_name", 
+            "last_name", 
+            "email"
+        )
+    
+    def validate_first_name(self, value):
+        print(value)
+        return value[0].upper() + value[1:]
